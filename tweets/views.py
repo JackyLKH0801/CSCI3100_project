561fe61d31d4318170a1fe0d933074c1186bb62e
JackyLKH0801 <jacky55314146@gmail.com>
2025-04-05 02:09:37 +0800

    Added dynamic url /tweet to view tweet id and content

--- tweets/views.py
+++ tweets/views.py
@@ -6,3 +6,11 @@ from .models import Tweet
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html', context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweet_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweet_list
+    }
+    return JsonResponse(data)
