0e54ded0021cc5bb5b32cf2188d86f5a7d9b6a6b
JackyLKH0801 <jacky55314146@gmail.com>
2025-04-05 01:41:22 +0800

    Added templates and home page testing

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404,JsonResponse
+
+from .models import Tweet
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    return render(request, 'pages/home.html', context={}, status=200)
