242e5cbb89848ad58c4b1c8ea350d64cf36f8d27
JackyLKH0801 <jacky55314146@gmail.com>
2025-04-07 00:21:25 +0800

    Added safe url redirect for POSTing

--- POSTer/settings.py
+++ POSTer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h5iomr2v%8ov&2^bdi4mp$r6fuxh_n_b=rictjb1brf19v+%l9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,14 @@
 import random
 
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404,JsonResponse
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Tweet
 from .forms import TweetForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
@@ -21,8 +25,11 @@ def tweet_list_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url != None and url_has_allowed_host_and_scheme(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form": form}, status=200)
