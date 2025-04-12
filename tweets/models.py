41eaddf5f464265079e5115b7aa2db920576be7f
JackyLKH0801 <jacky55314146@gmail.com>
2025-04-13 00:12:58 +0800

    Added like field and timestamp for like and tweet

--- tweets/models.py
+++ tweets/models.py
@@ -3,11 +3,22 @@ from django.conf import settings
 from django.db import models
 
 User = settings.AUTH_USER_MODEL
+
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey('Tweet', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True) #timestamp of when the tweet was liked
+    
+
+        
+        
 # Create your models here.
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) #delete default = 1 later. Code breaks if no default for some reason.
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, through=TweetLike)
+    timestamp = models.DateTimeField(auto_now_add=True) #timestamp of when the tweet was created
 
     class Meta:
         ordering = ['-id']
