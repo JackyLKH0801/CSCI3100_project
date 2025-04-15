1e1a28416466d67c97e36b564537df949913bbe3
PEKKACarl <aycarl2004@gmail.com>
2025-04-16 03:16:56 +0800

    like button and react event handling

--- poster-web/src/index.js
+++ poster-web/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import {React, StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { TweetsComponent } from './tweets';
+const rootContainer =document.getElementById('root');
+if (rootContainer){
+  const appEl = ReactDOM.createRoot(rootContainer)
+  appEl.render(
+    <StrictMode>
+      <App />
+      </StrictMode>
+  )
+}
+const container = document.getElementById('cfe-tweets')
+if (container){
+const tweetsEl = ReactDOM.createRoot(container);
+tweetsEl.render(
+  <StrictMode>
+<TweetsComponent />
+</StrictMode>
 );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
