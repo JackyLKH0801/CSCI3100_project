156ae8b2aed036e5f4731ceea36ad23c5b5ef521
SSuuuunnnnnyyy <2014sunnycsk@gmail.com>
2025-05-07 22:09:25 +0800

    Added tweet_feed_view

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -24,7 +24,20 @@ def tweet_detailed_view(request, tweet_id, *args, **kwargs):
     serializer = TweetSerializer(obj)
     return Response(serializer.data, status = 200)
 
-@api_view(['GET'])  #http method the client == POST
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def tweet_feed_view(request, *args, **kwargs):
+    user = request.user
+    profiles = user.following.all()
+    followed_users_id = []
+    if profiles.exists():
+        followed_users_id = [x.user.id for x in profiles]
+    followed_users_id.append(user.id)
+    qs = Tweet.objects.filter(user__id__in = followed_users_id).order_by("-timestamp")
+    serializer = TweetSerializer(qs, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
     username = request.GET.get('username')
