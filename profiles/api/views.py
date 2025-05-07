e556916fa0893715498495376f7a876d4cba1343
SSuuuunnnnnyyy <2014sunnycsk@gmail.com>
2025-05-07 21:16:52 +0800

    Setup follow endpoint

---
+++ profiles/api/views.py
@@ -0,0 +1,35 @@
+import random
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404,JsonResponse
+from django.utils.http import url_has_allowed_host_and_scheme
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
+from ..models import Profile
+
+User = get_user_model()
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated]) #only authenticated users can create tweets
+def user_follow_view(request, username, *args, **kwargs):
+    current_user = request.user
+    to_follow_user_qs = User.objects.filter(username = username)
+    if not to_follow_user_qs.exists():
+        return Response({}, status = 404)
+    to_follow_user = to_follow_user_qs.first()
+    profile = to_follow_user.profile
+    data = request.data or {}
+    action = data.get('action')
+    if action == 'follow':
+        profile.followers.add(current_user)
+    elif action == 'unfollow':
+        profile.followers.remove(current_user)
+    else:
+        pass
+    current_followers_qs = profile.followers.all()
+    return Response({"followers": current_followers_qs.count()}, status = 200)
